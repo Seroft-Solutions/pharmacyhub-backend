c918dd085dc712b49c89d368f2b1aa8fd2af5c85
Syed Usman <63198671+syedus06@users.noreply.github.com>
2025-03-15 09:45:58 +0500

    asdadsa

--- src/main/java/com/pharmacyhub/security/config/RBACCacheConfig.java
+++ src/main/java/com/pharmacyhub/security/config/RBACCacheConfig.java
@@ -6,6 +6,7 @@ import org.springframework.cache.concurrent.ConcurrentMapCache;
 import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import java.util.Arrays;
 
@@ -14,13 +15,25 @@ import java.util.Arrays;
 public class RBACCacheConfig {
 
     @Bean
-    public CacheManager rbacCacheManager() {
+    @Primary
+    public CacheManager cacheManager() {
         SimpleCacheManager cacheManager = new SimpleCacheManager();
         cacheManager.setCaches(Arrays.asList(
+            // Existing caches
             new ConcurrentMapCache("userPermissions"),
             new ConcurrentMapCache("userRoles"),
             new ConcurrentMapCache("roleHierarchy"),
-            new ConcurrentMapCache("groupRoles")
+            new ConcurrentMapCache("groupRoles"),
+            
+            // Feature access caches
+            new ConcurrentMapCache("featureAccess"),
+            new ConcurrentMapCache("featureOperations"),
+            new ConcurrentMapCache("featureOperationAccess"),
+            new ConcurrentMapCache("userFeatures"),
+            
+            // User permission caches
+            new ConcurrentMapCache("userHasPermission"),
+            new ConcurrentMapCache("userHasRole")
         ));
         return cacheManager;
     }
